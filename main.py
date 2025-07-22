ae1d49f871fb5154851ba3619767bd8b04e216df
Jordan-Stephens <JordanKeithStephens@gmail.com>
2025-07-22 18:53:39 -0400

    Turned into word counter

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+def word_counter(words):
+    word_count = 0
+    text = words.split()
+    for word in text:
+        word_count += 1
+    return word_count
+
 def main():
-    return print(get_book_text("books/frankenstein.txt"))
+    words = get_book_text("books/frankenstein.txt")
+    count = word_counter(words)
+    return print(f"{count} words found in the document")
+
 
 main()
